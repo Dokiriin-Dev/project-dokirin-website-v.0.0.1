bd3099a199b18763bd8863cd47cd75e77b035dbb
Dokiriin-Dev <85996922+Dokiriin-Dev@users.noreply.github.com>
2023-06-12 06:30:45 +0200

    fix classsi tailwind

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,7 @@
-import React from "react";
-import { auth } from "@/firebase/firebase.config";
 import Link from "next/link";
-import { useAuthState } from "react-firebase-hooks/auth";
-import Logo from "./logo";
+import React from "react";
 import { HiMenu } from "react-icons/hi";
+import Logo from "./logo";
 
 type MenuItemProps = {
   href: string;
@@ -23,7 +21,7 @@ const MenuItem = ({ href, children }: MenuItemProps) => {
   );
 };
 
-const ServiziMenu = () => {
+const ServiceMenu = () => {
   return (
     <div className="absolute top-full left-0 mt-0 rounded-sm shadow-lg z-10 w-40 bg-gradient-to-b from-zinc-800/60 backdrop-blur-2xl text-slate-300">
       <span className="block py-2 w-full rounded-sm p-6 cursor-pointer hover:text-[#FFAE00]">
@@ -41,18 +39,17 @@ const ServiziMenu = () => {
 };
 
 export default function Navbar() {
-  const [user] = useAuthState(auth);
-
   return (
     <header className="z-50 fixed left-0 top-0 flex w-full text-gray-700 body-font border-b border-gray-200 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:bg-gray-200 lg:dark:bg-zinc-800/30">
       <div className="container justify-between mx-auto flex flex-wrap md:p-5 p-2.5 flex-col md:flex-row items-center">
         <div className="flex items-center justify-between w-full sm:w-0">
-          <Logo className="ml-5 sm-0" />
+          <Logo className="ml-5 sm:ml-0" />
           <HiMenu
-            className="md:hidden hover:text-[#FFAE00] text-slate-300 mr-5 sm-0"
+            className="md:hidden hover:text-[#FFAE00] text-slate-300 mr-5 sm:mr-0"
             size={25}
           />
         </div>
+
         <div className="md:flex text-xl hidden justify-end">
           <MenuItem href="/">Home</MenuItem>
           <MenuItem href="/about">About</MenuItem>
@@ -61,13 +58,11 @@ export default function Navbar() {
               Servizi
             </span>
             <div className="group-hover:block hidden">
-              <ServiziMenu />
+              <ServiceMenu />
             </div>
           </div>
           <MenuItem href="/contacts">Contatti</MenuItem>
-          <MenuItem href={user ? "/dashboard" : "/auth/login"}>
-            {user ? "Dashboard" : "Area Clienti"}
-          </MenuItem>
+          <MenuItem href="/auth/login">Area Clienti</MenuItem>
         </div>
       </div>
     </header>
